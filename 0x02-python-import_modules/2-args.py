5a1afeb408a9f8e7b225d20d09836f4b75cc0744
Ikar700 <Cornex111@gmail.com>
2024-01-30 14:33:03 -0800

    bECAUSE I'M A PERFECTIONIST

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     count = len(sys.argv) - 1
 
     if count == 0:
-        print("0 argument.")
+        print("0 arguments.")
     elif count ==1 :
         print(f"{count} argument:")
     else:
         print(f"{count} arguments:")
 
     for i in range(count):
-        print(f"{i + 1}: {sys.argv[i + 1]}")
+        print(f"{i + 1} : {sys.argv[i + 1]}")
