76d2c94eb2573f85d3ec7c70e3c5a92f2ba662c8
Ikar700 <Cornex111@gmail.com>
2024-01-30 14:22:06 -0800

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python 
+
+if __name__ == "__main__":
+
+    import sys
+
+    count = len(sys.argv)
+
+    if count == 0:
+        print("0 argument.")
+    elif count ==1 :
+        print(f"{count} argument:")
+    else:
+        print(f"{count} arguments:")
+
+    for i in range(count):
+        print(f"{i+1}: {sys.argv[i+1]}")
