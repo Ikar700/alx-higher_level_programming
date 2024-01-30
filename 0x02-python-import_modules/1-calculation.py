0c1fabc902cc49ec3cf0acb2b468bbfd296cbcd7
Ikar700 <Cornex111@gmail.com>
2024-01-29 16:20:01 -0800

    lets see

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ if __name__ = "__main__":
     a = 10
     b = 5
 
-    from calculation_1 import add, sub, div, mul
+    from calculator_1 import add, sub, div, mul
 
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
